66aefb6e1b648bb6b70a765a45a289c39c25aa99
halynaburdiian <verdamtion@gmail.com>
2019-07-06 17:07:38 +0200

    implementation of display stats and display form function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,3 +1,4 @@
+
 // Write your JS here
 const hero = {
     name: "MYBESTHERO",
@@ -13,6 +14,7 @@ const hero = {
 function rest (obj){
     if (obj.health === 10) {
         alert("You don't need rest");
+        displayStats(hero)
     }
     else {
         obj.health = 10;
@@ -27,6 +29,7 @@ inn.onclick = function (){
 
 function pickUpItem(hero, weapon){
     hero.inventory.push(weapon);
+    displayStats(hero)
 
 }
 
@@ -39,6 +42,7 @@ dagger.onclick = function(){
 function equipWeapon(hero){
     if (hero.inventory.length != 0){
         hero.weapon = hero.inventory[0];
+        displayStats(hero)
     }
     else {
         return null
@@ -49,3 +53,45 @@ const bag = document.getElementById('bag');
 bag.onclick = function(){
     equipWeapon(hero);
 }
+
+function displayStats (hero) {
+    const myCurrentStat = document.getElementById("my-current-stat");
+    const name = document.createElement('h3');
+    name.innerHTML = "My hero name : "+ hero.name;
+    const health = document.createElement('h4');
+    health.innerHTML = "Health : " + hero.health;
+    const weapon = document.createElement('h4');
+    weapon.innerHTML = "My weapon : " + hero.weapon.type;
+    const weaponDamage = document.createElement('h4')
+    weaponDamage.innerHTML = "My weapon damage : " + hero.weapon.damage;
+    myCurrentStat.innerHTML = null;
+    
+    myCurrentStat.appendChild(name)
+    myCurrentStat.appendChild(health)
+    myCurrentStat.appendChild(weapon)
+    myCurrentStat.appendChild(weaponDamage)
+    name.onclick = function(){
+        displayForm(name)
+    }
+}
+let formDisplayed = false;
+
+function displayForm(name){
+    if (formDisplayed === true){
+        return
+    }
+    const form = document.createElement('form');
+    const inputField = document.createElement('input');
+    inputField.name = 'changeName';
+    form.appendChild(inputField)
+    const parent = name.parentNode;
+    parent.insertBefore(form,name);
+    formDisplayed = true;
+    form.onsubmit = function(){
+        hero.name = this.changeName.value ;
+        displayStats(hero)
+        formDisplayed = false;
+        return false
+    }
+}
+displayStats(hero)
